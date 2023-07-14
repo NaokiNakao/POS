5aa2733b73fde630ce7f18af3f77e72ac7489058
Naoki Nakao <naokinakao.nn@gmail.com>
2023-07-13 22:24:51 -0400

    Configure Web Security

---
+++ src/main/java/com/nakao/pos/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.nakao.pos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author Naoki Nakao on 7/13/2023
+ * @project POS
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/**").permitAll();
+                })
+                .build();
+    }
+
+}
